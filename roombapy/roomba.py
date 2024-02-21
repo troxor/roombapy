41ab53a230b6a2eaee10eedf502fb71379f3d510
Joost Lekkerkerker <joostlek@outlook.com>
2024-02-21 18:38:25 +0100

    Fix SIM102 (#253)
    Signed-off-by: Joostlek <joostlek@outlook.com>

--- roombapy/roomba.py
+++ roombapy/roomba.py
@@ -180,9 +180,8 @@ class Roomba:
 
     def on_message(self, _mosq, _obj, msg):
         """On message callback."""
-        if self.exclude != "":
-            if self.exclude in msg.topic:
-                return
+        if self.exclude != "" and self.exclude in msg.topic:
+            return
 
         if self.indent == 0:
             self.master_indent = max(self.master_indent, len(msg.topic))
